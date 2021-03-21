6bf3d456b20ba3f18ed0163acae22a20075292de
Elise Hoovler <elisejhoovler@live.com>
2021-03-20 21:51:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Commerce from '@chec/commerce.js';
+import Item from './components/Item';
+
+const commerce = new Commerce(
+  "pk_24914c50e3f6ecb8d48c8ad0a1926c6b9dd6e6cf8c500"
+);
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    commerce.products.list().then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.map((product) => (
+        <Item key={product.id} {...product} />
+      ))}
     </div>
   );
 }
